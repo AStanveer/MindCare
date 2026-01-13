4275cc1878cf490ad1a24585003906e7986735b0
MoazJalal02 <moazadil02@gmail.com>
2026-01-13 17:33:59 +0800

    fix wrong links issue

--- src/main/java/com/teamspring/MindCare/controller/DashboardController.java
+++ src/main/java/com/teamspring/MindCare/controller/DashboardController.java
@@ -92,7 +92,7 @@ public class DashboardController {
         List<QuickAction> actions = List.of(
             new QuickAction("Mood Tracker", "icon-mood-2", "/mindcare/mood/tracker"),
             new QuickAction("Self Care", "icon-self-care", "/mindcare/selfcare"),
-            new QuickAction("Book Session", "icon-book-session", "/mindcare/booking"),
+            new QuickAction("Book Session", "icon-book-session", "/mindcare/counselling/booking"),
             new QuickAction("Peer Support", "icon-peer-support", "/mindcare/peer-support")
         );
         
